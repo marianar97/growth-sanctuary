81f14ba660b4ba6e994a4db0be459176e68c22e6
marianar97 <mariana.ramirezd97@gmail.com>
2025-07-27 01:16:01 -0500

    refactor: update type definitions in Search component to support flexible tag handling

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,7 +1,7 @@
 import SearchBar from "./SearchBar"
 import TagSelector from "./TagSelector"
 
-export default function Search({ onSearch, tags, onTagChange }: { onSearch: (query: string) => void, tags: string[], onTagChange: (tag: string) => void }) {
+export default function Search({ onSearch, tags, onTagChange }: { onSearch: (query: string) => void, tags: any[], onTagChange: (tags: any[]) => void }) {
   return (
     <div className="flex flex-col gap-2 w-full">
       <SearchBar onSearch={onSearch} />
